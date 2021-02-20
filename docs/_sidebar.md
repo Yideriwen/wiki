551c236badc5646723deda875ad5b592bf052be6
adamyide <eaderun@gmail.com>
2021-02-20 09:11:40 +0800

    +.gitignore

---
+++ docs/_sidebar.md
@@ -0,0 +1,10 @@
+* Python
+
+	* [Python_wiki](Python/Python_wiki.md)
+* 小套路
+* Mental_Models
+
+	* [.DS_St](Mental_Models/.DS_Store)
+* Pandas
+
+	* [README](Pandas/README.md)
---
+++ docs/make_sidebar.py
@@ -0,0 +1,19 @@
+import os
+
+path = '.'
+fs = os.listdir(path)
+
+folders = []
+
+for i in fs:
+    if os.path.isdir(i):
+        folders.append(i)
+
+target_file = './_sidebar.md'
+
+file = open(target_file, 'w')
+for i in folders:
+    file.write('* '+i+'\n')
+    for j in os.listdir('./'+i):
+        file.write('\n\t'+'* '+'['+j[:-3]+']'+'('+i+'/'+j+')'+'\n')
+file.close()
